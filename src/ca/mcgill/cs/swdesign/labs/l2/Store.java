28158d1865e1b99e896818201e2668a582d504dd
huetimes3 <ttricamavrodin@gmail.com>
2023-04-03 13:21:33 -0400

    changed stuff

--- src/ca/mcgill/cs/swdesign/labs/l2/Store.java
+++ src/ca/mcgill/cs/swdesign/labs/l2/Store.java
@@ -1,14 +1,11 @@
 package ca.mcgill.cs.swdesign.labs.l2;
 
 public class Store {
-    private SellableThing[] aSellableThings;
-
     public Store(SellableThing[] pSellableThings) {
         SellableThing[] cSellableThings = new SellableThing[pSellableThings.length];
         for (int i = 0; i < pSellableThings.length; i++) {
             cSellableThings[i] = pSellableThings[i].clone();
         }
-        aSellableThings = cSellableThings;
     }
 
     
